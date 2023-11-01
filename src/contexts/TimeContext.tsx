c7c5b8f4453fa13fcc6e86851ab3013e877ea3b5
kam219kat <techo219@proton.me>
2023-11-01 05:34:13 +0100

    Update documentation

--- src/contexts/TimeContext.tsx
+++ src/contexts/TimeContext.tsx
@@ -8,27 +8,38 @@ import {
 } from 'react';
 import { ClockState } from '../store/reducers/clockReducer';
 
+/**
+ * The type for the TimeContext.
+ */
 type TimeContextType = ClockState;
 
+/**
+ * Props for the TimeContextProvider component.
+ */
 type TimeContextProviderProps = {
   children: ReactElement
 };
+
 /**
- * The context for the current time.
+ * The default state for the TimeContext.
  */
-const defaultState = {
+const defaultState: TimeContextType = {
   hours: 0,
   minutes: 0,
   seconds: 0,
 };
 
+/*
+ * The TimeContext.
+ */
 const TimeContext = createContext<TimeContextType>(defaultState);
 
 /**
  * The provider component for the TimeContext.
- * @param children - The children components.
+ * @param {TimeContextProviderProps} props - The component props.
+ * @returns {React.ReactElement} - The TimeContextProvider component.
  */
-export function TimeContextProvider({ children }: TimeContextProviderProps) {
+export function TimeContextProvider({ children }: TimeContextProviderProps): React.ReactElement {
   return (
     <TimeContext.Provider value={defaultState}>
       {children}
