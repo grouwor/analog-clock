f994a82ca9de8840493f197876e4b025d387b067
kam219kat <techo219@proton.me>
2023-11-01 07:24:08 +0100

    Import App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ClockContextProvider } from './contexts/ClockContext';
 import ClockContainer from './components/ClockContainer';
-
+import './App.css';
 /**
  * App component is the root component of the application
  */
