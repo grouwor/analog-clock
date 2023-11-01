e91311abe3dc912637af4b9c91efad63a9fa7183
kam219kat <techo219@proton.me>
2023-11-01 06:07:10 +0100

    Combine TimeContext with the ClockReducer

--- src/contexts/TimeContext.tsx
+++ src/contexts/TimeContext.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
-import {
+import React, {
+  useReducer,
   createContext,
   ReactNode,
   Dispatch,
   SetStateAction,
   ReactElement
 } from 'react';
-import { ClockState } from '../store/reducers/clockReducer';
+import * as ClockActions from '../store/actions/clockActions';
+import {
+  ClockReducer,
+  ClockState,
+  initialState as InitialClockState
+} from '../store/reducers/clockReducer';
+import { ClockAction } from '../store/actions/clockActions';
 
 /**
- * The type for the TimeContext.
+ * Defines the shape of the time context object.
  */
-type TimeContextType = ClockState;
+interface TimeContextType extends ClockState {
+  /**
+   * Handles the tick action and updates the time state.
+   * @param {Date} date - The current date.
+   * @returns {void}
+   */
+  handleTick(date: Date): void;
+};
 
 /**
  * Props for the TimeContextProvider component.
@@ -20,19 +33,10 @@ type TimeContextProviderProps = {
   children: ReactElement
 };
 
-/**
- * The default state for the TimeContext.
- */
-const defaultState: TimeContextType = {
-  hours: 0,
-  minutes: 0,
-  seconds: 0,
-};
-
 /*
  * The TimeContext.
  */
-const TimeContext = createContext<TimeContextType>(defaultState);
+const TimeContext = createContext<TimeContextType | null>(null);
 
 /**
  * The provider component for the TimeContext.
@@ -40,8 +44,32 @@ const TimeContext = createContext<TimeContextType>(defaultState);
  * @returns {React.ReactElement} - The TimeContextProvider component.
  */
 export function TimeContextProvider({ children }: TimeContextProviderProps): React.ReactElement {
+  // Initialize the time state using the ClockReducer and InitialClockState.
+  const [time, dispatch]: [ClockState, Dispatch<ClockAction>] = useReducer(
+    ClockReducer,
+    InitialClockState
+  );
+
+  /**
+   * Handles the tick action and updates the time state.
+   * @param {Date} date - The current date.
+   * @returns {void}
+   */
+  const handleTick = (date: Date): void => {
+    // Dispatch the tick action with the current hour, minute, and second values from the date parameter.
+    dispatch(ClockActions.tick(
+      date.getHours(),
+      date.getMinutes(),
+      date.getSeconds()
+    ));
+  }
+
+  // Wrap the children components with the TimeContext.Provider component, passing the time state and handleTick function as the context value.
   return (
-    <TimeContext.Provider value={defaultState}>
+    <TimeContext.Provider value={{
+      ...time,
+      handleTick
+    }}>
       {children}
     </TimeContext.Provider>
   );
